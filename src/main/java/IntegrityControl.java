9d2023fa2f8ee9ae63edf6a9802c0e2b7e56a607
Lionel Tougne <lgt_23@msn.com>
2015-09-22 22:37:36 +0200

    complete the lunhn test

--- src/main/java/IntegrityControl.java
+++ src/main/java/IntegrityControl.java
@@ -1,4 +1,9 @@
+import com.google.common.collect.ImmutableList;
 import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * Created by lionel on 20/09/2015.
@@ -6,12 +11,62 @@ import org.apache.commons.lang3.StringUtils;
 public class IntegrityControl {
 
 
-    public void prepareString(String entry) throws ArgumentException {
-        if(StringUtils.isBlank(entry)){
+    public boolean calculateLuhn(String entry) throws ArgumentException {
+        if (StringUtils.isBlank(entry)) {
             throw new ArgumentException("argument should be a non blank character sequence like '1121323232323'");
         }
 
+        List<Integer> myListOfNumberToTest = parseStringToNumber(entry);
 
 
+        ImmutableList<Object> listOfValue = ImmutableList.builder().addAll(myListOfNumberToTest).build();
+
+        ImmutableList reverlistOfValue =  listOfValue.reverse();
+
+        IntStream instreamToCalculate = generateEvenIndexSequence(reverlistOfValue.size());
+
+        int sum = calculateSumOfEvenDigitsOfSequence(reverlistOfValue);
+
+        int sumofodddigit  = calculateSumOfOddDigitsOfSequence(reverlistOfValue);
+
+        return isValid(sum,sumofodddigit);
+
+    }
+
+    public boolean isValid(int sum,int sumofodddigit){
+        int sommeTotal  = sum + sumofodddigit;
+
+        return sommeTotal%10 == 0 ? true : false;
+    }
+
+    public int calculateSumOfEvenDigitsOfSequence(ImmutableList listOfValue){
+        return generateEvenIndexSequence(listOfValue.size())
+                .mapToObj(indexNumber -> ((Integer) listOfValue.get(indexNumber)))
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    public int calculateSumOfOddDigitsOfSequence(ImmutableList listOfValue){
+        return generateOddIndexSequence(listOfValue.size()).boxed()
+                .mapToInt(indexNumber -> ((Integer) listOfValue.get(indexNumber)).intValue() * 2)
+                .boxed()
+                .mapToInt(x -> getsum(x))
+                .sum();
+    }
+
+    public List<Integer> parseStringToNumber(String entry){
+        return IntStream.range(0, entry.length()).mapToObj(index -> new Integer(entry.substring(index, index + 1))).collect(Collectors.toList());
+    }
+
+    public IntStream generateEvenIndexSequence(int sizeOfTheList){
+        return IntStream.iterate(0,i -> i+2).limit(sizeOfTheList).filter(x -> x < sizeOfTheList);
+    }
+
+    public IntStream generateOddIndexSequence(int sizeOfTheList){
+        return IntStream.iterate(1,i -> i+2).limit(sizeOfTheList).filter(x -> x < sizeOfTheList);
+    }
+
+    public int getsum(int n) {
+        return n == 0 ? 0 : n % 10 + getsum(n/10);
     }
 }
--- src/test/java/IntegrityControlTest.java
+++ src/test/java/IntegrityControlTest.java
@@ -1,5 +1,12 @@
-import junit.framework.TestCase;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.stream.IntStream;
 
 /**
  * Created by lionel on 20/09/2015.
@@ -11,7 +18,95 @@ public class IntegrityControlTest{
     public void emptyOrNullStringShouldReturnAnError() throws ArgumentException {
 
         IntegrityControl testControl = new IntegrityControl();
-        testControl.prepareString(null);
+        testControl.calculateLuhn(null);
 
     }
+
+    @Test
+    public void testThatTHeGeneratorGetAValue(){
+        IntegrityControl testControl = new IntegrityControl();
+        //We have a list with 8 elements inside
+        final IntStream intStreamResult = testControl.generateEvenIndexSequence(8);
+
+        //The result shoud be a list with 0 2 4 6
+        Assertions.assertThat(intStreamResult.toArray()).isEqualTo(Arrays.asList(0, 2, 4, 6).toArray());
+
+    }
+
+
+    @Test
+    public void testThatTHeOddGeneratorGetAValue(){
+        IntegrityControl testControl = new IntegrityControl();
+        //We have a list with 8 elements inside
+        final IntStream intStreamResult = testControl.generateOddIndexSequence(8);
+
+        //The result shoud be a list with 1 3 5 7
+        Assertions.assertThat(intStreamResult.toArray()).isEqualTo(Arrays.asList(1, 3, 5, 7).toArray());
+
+    }
+
+    @Test
+    public void testThatTHeOddGeneratorGetAValueFromSize11(){
+        IntegrityControl testControl = new IntegrityControl();
+        //We have a list with 8 elements inside
+        final IntStream intStreamResult = testControl.generateOddIndexSequence(11);
+
+        //The result shoud be a list with 1 3 5 7
+        Assertions.assertThat(intStreamResult.toArray()).isEqualTo(Arrays.asList(1, 3, 5, 7,9).toArray());
+
+    }
+
+    @Test
+    public void testParseString(){
+        String stringOfInt = "123456";
+        IntegrityControl testControl = new IntegrityControl();
+        List<Integer> listOfInteger = testControl.parseStringToNumber(stringOfInt);
+        Assertions.assertThat(listOfInteger).isEqualTo(Lists.newArrayList(1, 2, 3, 4, 5, 6));
+    }
+
+    @Test
+    public void shouldAZeroIntegerReturnASumOfZero(){
+        IntegrityControl testControl = new IntegrityControl();
+        int somme = testControl.getsum(0);
+
+        Assertions.assertThat(somme).isEqualTo(0);
+    }
+
+    @Test
+    public void should280ReturnASumOf10(){
+        IntegrityControl testControl = new IntegrityControl();
+        int somme = testControl.getsum(280);
+
+        Assertions.assertThat(somme).isEqualTo(10);
+    }
+
+    @Test
+    public void shouldSequence123456return9(){
+        IntegrityControl testControl = new IntegrityControl();
+        ImmutableList testList = new ImmutableList.Builder<Integer>().add(1,2,3,4,5,6).build();
+        int somme  = testControl.calculateSumOfEvenDigitsOfSequence(testList);
+
+        Assertions.assertThat(somme).isEqualTo(9);
+    }
+
+    @Test
+    public void shouldSequence123456return15(){
+        IntegrityControl testControl = new IntegrityControl();
+        ImmutableList testList = new ImmutableList.Builder<Integer>().add(1,2,3,4,5,6).build();
+        int somme  = testControl.calculateSumOfOddDigitsOfSequence(testList);
+
+        Assertions.assertThat(somme).isEqualTo(15);
+    }
+
+    @Test
+    public void should49927398716isvalid() throws ArgumentException {
+
+
+        IntegrityControl testControl = new IntegrityControl();
+        boolean result = testControl.calculateLuhn("49927398716");
+
+        Assertions.assertThat(result).isTrue();
+
+    }
+
 }
